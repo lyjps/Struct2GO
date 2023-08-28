b95621ab9bdae0c62a6de4c2b0155bdf565acba4
lyjps <92791137+lyjps@users.noreply.github.com>
2023-08-29 01:31:03 +0800

    Update evaluation.py

--- model/evaluation.py
+++ model/evaluation.py
@@ -1,8 +1,22 @@
 from sklearn import metrics
 from sklearn.metrics import roc_auc_score, roc_curve, auc, precision_score, recall_score, f1_score, average_precision_score
 import numpy as np
+import dgl
 from tkinter import _flatten
 
+
+
+def update_parent_features(label_network:dgl.DGLGraph, labels):
+    # 获取图中的所有边
+    edges = label_network.edges()
+    # 对于图中的每条边
+    for child_idx, parent_idx in zip(edges[0], edges[1]):
+        # 如果child节点的特征值大于parent节点的特征值
+        if labels[0][child_idx] > labels[0][parent_idx]:
+            # 更新parent节点的特征值为child节点的特征值
+            labels[0][parent_idx] = labels[0][child_idx]
+        # 更新labels的第二列为second_dim_elements
+    return labels
 
 def cacul_aupr(lables, pred):
     precision, recall, _thresholds = metrics.precision_recall_curve(lables, pred)
@@ -10,12 +24,13 @@ def cacul_aupr(lables, pred):
     aupr += metrics.auc(recall, precision)
     return aupr
 
-def calculate_performance(actual, pred_prob, threshold=0.2, average='micro'):
+def calculate_performance(actual, pred_prob, label_network:dgl.DGLGraph, threshold=0.2, average='micro'):
     pred_lable = []
     actual_label = []
     for l in range(len(pred_prob)):
         eachline = (np.array(pred_prob[l]) > threshold).astype(np.int)
         eachline = eachline.tolist()
+        # eachline = update_parent_features(label_network,eachline)
         pred_lable.append(list(_flatten(eachline)))
     for l in range(len(actual)):
         eachline = (np.array(actual[l])).astype(np.int)
